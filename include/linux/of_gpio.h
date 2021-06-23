boundarydevices/linux

gpio: gpiolib: add GPIO_PULSE_HIGH
Signed-off-by: Troy Kisky <troy.kisky@boundarydevices.com>

include/linux/of_gpio.h
@@ -32,6 +32,7 @@ enum of_gpio_flags {
 	OF_GPIO_SINGLE_ENDED = 0x2,
 	OF_GPIO_OPEN_DRAIN = 0x4,
 	OF_GPIO_SLEEP_MAY_LOOSE_VALUE = 0x8,
+	OF_GPIO_PULSE_HIGH = 0x10,
 };
 
 #ifdef CONFIG_OF_GPIO

SoarHuang/linux-imx6

include/linux/of_gpio.h
@@ -29,6 +29,7 @@ enum of_gpio_flags {
 	OF_GPIO_TRANSITORY = 0x8,
 	OF_GPIO_PULL_UP = 0x10,
 	OF_GPIO_PULL_DOWN = 0x20,
+	OF_GPIO_PULSE_HIGH = 0x40,
 };
 
 #ifdef CONFIG_OF_GPIO